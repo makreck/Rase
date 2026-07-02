d541fcf428e6db572e7491f8966a6cc53d34799a
makreck <makreck@outlook.de>
2026-07-02 10:56:39 +0200

    Added a MQTT base class.

---
+++ firmware/esp32/include/mqtt.hpp
@@ -0,0 +1,38 @@
+/*
+ * ==============================================================================
+ *
+ *  PROJECT:     "Rase" Radio Sensor Project,    ESP32-S3 Station Device Firmware
+ *  COPYRIGHT:   (C)2025-2026 KKS-Elektronik,  M. Kreck, <makreck@googlemail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful,   but WITHOUT
+ *  ANY WARRANTY, without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE, see the GNU General Public License for details.
+ *
+ *  You should have received a copy of the  GNU General Public License along with
+ *  this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ==============================================================================
+ */
+
+#pragma once
+
+class Mqtt {
+    private:
+        void init(void);
+        void cleanup(void);
+
+public:
+        Mqtt(void) {
+            init();
+        }
+
+        ~Mqtt() {
+            cleanup();
+        }
+
+};
