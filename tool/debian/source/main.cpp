7abce6bf3b44ff11fb00bc8d83a62352eb3f37e8
makreck <makreck@outlook.de>
2026-07-13 11:39:24 +0200

    Corrected help text.

--- tool/debian/source/main.cpp
+++ tool/debian/source/main.cpp
@@ -39,7 +39,7 @@ void App::print_help(void) {
     printf("Supported commands:\n");
     printf("\t\"/connect=<ssid>:<password>\"\tConnect to a WLAN access point.\n");
     printf("\t\"/broker=<MQTT broker>:<username>:<password>\"\tConfigure a MQTT broker/server link.\n");
-    printf("\t\"/config=<JSON.file>\"\t\tConfigure device by given JSON string.\n");
+    printf("\t\"/config=<JSON.file>\"\t\tConfigure device by given JSON file.\n");
     printf("\t\"/initialize\"\t\t\tPerform a factory reset.\n");
     printf("\t\"/reboot\"\t\t\tReboot the device.\n");
     printf("\t\"/root\"\t\t\t\tQuery the root website source.\n");
