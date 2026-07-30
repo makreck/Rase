a7408cf94e530044724eb39770b98f1d3f323f2c
makreck <makreck@outlook.de>
2026-07-30 16:50:29 +0200

    Extended event codes with display_lock and display_unlock.

--- firmware/esp32/include/events.hpp
+++ firmware/esp32/include/events.hpp
@@ -31,6 +31,8 @@ enum class AppEvent:uint32_t {
     display_ready     = 100,
     display_off       = 101,
     display_config    = 102,
+    display_lock      = 103,
+    display_unlock    = 104,
     
     wifi_enabled      = 200,
     wifi_connected    = 201,
