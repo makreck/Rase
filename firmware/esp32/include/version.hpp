bdb6a24a457af75325bd8116e2e7e86baaafb3f1
makreck <makreck@outlook.de>
2026-08-03 08:22:53 +0200

    Version numbers increased.

--- firmware/esp32/include/version.hpp
+++ firmware/esp32/include/version.hpp
@@ -26,7 +26,7 @@
 #define VS_HIGH                (0)
 #define VS_LOW                 (0)
 #define VS_REV                 (1)
-#define VS_BUILD               (6)
+#define VS_BUILD               (7)
 
 #define VERSION_BUILD          (((VS_HIGH  << 24) & 0xff000000) | \
                                 ((VS_LOW   << 16) & 0x00ff0000) | \
